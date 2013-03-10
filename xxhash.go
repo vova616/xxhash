9f4e9b2e7d3f988deaf3f11dd260fc8954fd71ed
Vova <vova616@gmail.com>
2013-03-10 09:55:14 +0200

    First commit.

---
+++ xxhash.go
@@ -0,0 +1,324 @@
+package xxhash
+
+import (
+	"errors"
+	"hash"
+)
+
+const (
+	PRIME32_1 = 2654435761
+	PRIME32_2 = 2246822519
+	PRIME32_3 = 3266489917
+	PRIME32_4 = 668265263
+	PRIME32_5 = 374761393
+)
+
+type XXHash struct {
+	seed, v1, v2, v3, v4 uint32
+	total_len            uint64
+	memory               [16]byte
+	memsize              int
+}
+
+func New(seed uint32) hash.Hash32 {
+	return &XXHash{
+		seed: seed,
+		v1:   seed + PRIME32_1 + PRIME32_2,
+		v2:   seed + PRIME32_2,
+		v3:   seed,
+		v4:   seed - PRIME32_1,
+	}
+}
+
+func (self *XXHash) BlockSize() int {
+	return 1
+}
+
+// Size returns the number of bytes Sum will return.
+func (self *XXHash) Size() int {
+	return 4
+}
+
+// Sum32 returns xxhash32 checksum and free internal state.
+func (self *XXHash) feed(in []byte) uint32 {
+	p := in
+	l := len(in)
+	l2 := l
+
+	self.total_len += uint64(l)
+
+	// fill in tmp buffer
+	if self.memsize+l < 16 {
+		copy(self.memory[self.memsize:], in)
+		self.memsize += l
+		return 0
+	}
+
+	if self.memsize > 0 {
+		copy(self.memory[self.memsize:], in[:16-self.memsize])
+		var p32 uint32 = uint32(self.memory[0]) |
+			(uint32(self.memory[1]) << 8) |
+			(uint32(self.memory[2]) << 16) |
+			(uint32(self.memory[3]) << 24)
+
+		self.v1 += p32 * PRIME32_2
+		self.v1 = ((self.v1 << 13) | (self.v1 >> (32 - 13)))
+		self.v1 *= PRIME32_1
+
+		p32 = uint32(self.memory[4]) |
+			(uint32(self.memory[5]) << 8) |
+			(uint32(self.memory[6]) << 16) |
+			(uint32(self.memory[7]) << 24)
+
+		self.v2 += p32 * PRIME32_2
+		self.v2 = ((self.v2 << 13) | (self.v2 >> (32 - 13)))
+		self.v2 *= PRIME32_1
+
+		p32 = uint32(self.memory[8]) |
+			(uint32(self.memory[9]) << 8) |
+			(uint32(self.memory[10]) << 16) |
+			(uint32(self.memory[11]) << 24)
+
+		self.v3 += p32 * PRIME32_2
+		self.v3 = ((self.v3 << 13) | (self.v3 >> (32 - 13)))
+		self.v3 *= PRIME32_1
+
+		p32 = uint32(self.memory[12]) |
+			(uint32(self.memory[13]) << 8) |
+			(uint32(self.memory[14]) << 16) |
+			(uint32(self.memory[15]) << 24)
+
+		self.v4 += p32 * PRIME32_2
+		self.v4 = ((self.v4 << 13) | (self.v4 >> (32 - 13)))
+		self.v4 *= PRIME32_1
+
+		s := 16 - self.memsize
+		p = p[s:]
+		self.memsize = 0
+
+		l2 -= s
+	}
+
+	limit := l2 - 16
+	v1, v2, v3, v4 := self.v1, self.v2, self.v3, self.v4
+
+	i := 0
+	for ; i <= limit; i += 16 {
+		var p32 uint32 = uint32(p[i]) |
+			(uint32(p[i+1]) << 8) |
+			(uint32(p[i+2]) << 16) |
+			(uint32(p[i+3]) << 24)
+
+		v1 += p32 * PRIME32_2
+		v1 = ((v1 << 13) | (v1 >> (32 - 13)))
+		v1 *= PRIME32_1
+
+		p32 = uint32(p[i+4]) |
+			(uint32(p[i+5]) << 8) |
+			(uint32(p[i+6]) << 16) |
+			(uint32(p[i+7]) << 24)
+
+		v2 += p32 * PRIME32_2
+		v2 = ((v2 << 13) | (v2 >> (32 - 13)))
+		v2 *= PRIME32_1
+
+		p32 = uint32(p[i+8]) |
+			(uint32(p[i+9]) << 8) |
+			(uint32(p[i+10]) << 16) |
+			(uint32(p[i+11]) << 24)
+
+		v3 += p32 * PRIME32_2
+		v3 = ((v3 << 13) | (v3 >> (32 - 13)))
+		v3 *= PRIME32_1
+
+		p32 = uint32(p[i+12]) |
+			(uint32(p[i+13]) << 8) |
+			(uint32(p[i+14]) << 16) |
+			(uint32(p[i+15]) << 24)
+
+		v4 += p32 * PRIME32_2
+		v4 = ((v4 << 13) | (v4 >> (32 - 13)))
+		v4 *= PRIME32_1
+	}
+
+	l2 -= i
+
+	if l2 > 0 {
+		copy(self.memory[:], p[i:i+l2])
+		self.memsize = l2
+	}
+
+	self.v1 = v1
+	self.v2 = v2
+	self.v3 = v3
+	self.v4 = v4
+
+	return 0
+}
+
+func (self *XXHash) Sum32() uint32 {
+	p := 0
+	bEnd := self.memsize
+	h32 := uint32(0)
+
+	if self.total_len >= 16 {
+		h32 = ((self.v1 << 1) | (self.v1 >> (32 - 1))) +
+			((self.v2 << 7) | (self.v2 >> (32 - 7))) +
+			((self.v3 << 12) | (self.v3 >> (32 - 12))) +
+			((self.v4 << 18) | (self.v4 >> (32 - 18)))
+	} else {
+		h32 = self.seed + PRIME32_5
+	}
+
+	h32 += uint32(self.total_len)
+
+	for p <= bEnd-4 {
+		var p32 uint32 = uint32(self.memory[p]) |
+			(uint32(self.memory[p+1]) << 8) |
+			(uint32(self.memory[p+2]) << 16) |
+			(uint32(self.memory[p+3]) << 24)
+		h32 += p32 * PRIME32_3
+		h32 = ((h32 << 17) | (h32 >> (32 - 17))) * PRIME32_4
+		p += 4
+	}
+
+	for p < bEnd {
+		h32 += uint32(self.memory[p]) * PRIME32_5
+		h32 = ((h32 << 11) | (h32 >> (32 - 11))) * PRIME32_1
+		p++
+	}
+
+	h32 ^= h32 >> 15
+	h32 *= PRIME32_2
+	h32 ^= h32 >> 13
+	h32 *= PRIME32_3
+	h32 ^= h32 >> 16
+
+	return h32
+}
+
+func (self *XXHash) Sum(in []byte) []byte {
+	h := self.Sum32()
+	in = append(in, byte(h>>24))
+	in = append(in, byte(h>>16))
+	in = append(in, byte(h>>8))
+	in = append(in, byte(h))
+	return in
+}
+
+func (self *XXHash) Reset() {
+	seed := self.seed
+	self.v1 = seed + PRIME32_1 + PRIME32_2
+	self.v2 = seed + PRIME32_2
+	self.v3 = seed
+	self.v4 = seed - PRIME32_1
+	self.total_len = 0
+	self.memsize = 0
+	self.memory = [16]byte{}
+}
+
+// Write adds more data to the running hash.
+// Length of data MUST BE less than 1 Gigabytes.
+func (self *XXHash) Write(data []byte) (nn int, err error) {
+	l := len(data)
+	if l > 1<<30 {
+		return 0, errors.New("Cannot add more than 1 Gigabytes at once.")
+	}
+	self.feed(data)
+	return len(data), nil
+}
+
+func Checksum32(data []byte) uint32 {
+	return Checksum32Seed(data, 0)
+}
+
+func Checksum32Seed(data []byte, seed uint32) uint32 {
+	p := 0
+	l := len(data)
+	bEnd := l
+	h32 := uint32(0)
+
+	if bEnd >= 16 {
+		limit := bEnd - 16
+
+		v1 := seed + PRIME32_1 + PRIME32_2
+		v2 := seed + PRIME32_2
+		v3 := seed + 0
+		v4 := seed - PRIME32_1
+		for {
+			var p32 uint32 = uint32(data[p]) |
+				(uint32(data[p+1]) << 8) |
+				(uint32(data[p+2]) << 16) |
+				(uint32(data[p+3]) << 24)
+
+			v1 += p32 * PRIME32_2
+			v1 = ((v1 << 13) | (v1 >> (32 - 13)))
+			v1 *= PRIME32_1
+
+			p32 = uint32(data[p+4]) |
+				(uint32(data[p+5]) << 8) |
+				(uint32(data[p+6]) << 16) |
+				(uint32(data[p+7]) << 24)
+
+			v2 += p32 * PRIME32_2
+			v2 = ((v2 << 13) | (v2 >> (32 - 13)))
+			v2 *= PRIME32_1
+
+			p32 = uint32(data[p+8]) |
+				(uint32(data[p+9]) << 8) |
+				(uint32(data[p+10]) << 16) |
+				(uint32(data[p+11]) << 24)
+
+			v3 += p32 * PRIME32_2
+			v3 = ((v3 << 13) | (v3 >> (32 - 13)))
+			v3 *= PRIME32_1
+
+			p32 = uint32(data[p+12]) |
+				(uint32(data[p+13]) << 8) |
+				(uint32(data[p+14]) << 16) |
+				(uint32(data[p+15]) << 24)
+
+			v4 += p32 * PRIME32_2
+			v4 = ((v4 << 13) | (v4 >> (32 - 13)))
+			v4 *= PRIME32_1
+
+			p += 16
+
+			if p > limit {
+				break
+			}
+		}
+		h32 = ((v1 << 1) | (v1 >> (32 - 1))) +
+			((v2 << 7) | (v2 >> (32 - 7))) +
+			((v3 << 12) | (v3 >> (32 - 12))) +
+			((v4 << 18) | (v4 >> (32 - 18)))
+	} else {
+		h32 = seed + PRIME32_5
+	}
+
+	h32 += uint32(l)
+
+	for p <= bEnd-4 {
+		var p32 uint32 = uint32(data[p]) |
+			(uint32(data[p+1]) << 8) |
+			(uint32(data[p+2]) << 16) |
+			(uint32(data[p+3]) << 24)
+		h32 += p32 * PRIME32_3
+		h32 = ((h32 << 17) | (h32 >> (32 - 17))) * PRIME32_4
+		p += 4
+	}
+
+	for p < bEnd {
+		h32 += uint32(data[p]) * PRIME32_5
+		h32 = ((h32 << 11) | (h32 >> (32 - 11))) * PRIME32_1
+		p++
+	}
+
+	h32 ^= h32 >> 15
+	h32 *= PRIME32_2
+	h32 ^= h32 >> 13
+	h32 *= PRIME32_3
+	h32 ^= h32 >> 16
+
+	return h32
+}
---
+++ xxhash_test.go
@@ -0,0 +1,143 @@
+package xxhash
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"hash/adler32"
+	"hash/crc32"
+	"hash/fnv"
+	"testing"
+)
+
+var (
+	blob1       = []byte("Lorem ipsum dolor sit amet, consectetuer adipiscing elit, ")
+	blob2       = []byte("sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.")
+	VeryBigFile = "a-very-big-file"
+)
+
+// MurmurHash 3
+// mmh3.Hash32 stollen from https://github.com/reusee/mmh3
+func mmh3Hash32(key []byte) uint32 {
+	length := len(key)
+	if length == 0 {
+		return 0
+	}
+	var c1, c2 uint32 = 0xcc9e2d51, 0x1b873593
+	nblocks := length / 4
+	var h, k uint32
+	buf := bytes.NewBuffer(key)
+	for i := 0; i < nblocks; i++ {
+		binary.Read(buf, binary.LittleEndian, &k)
+		k *= c1
+		k = (k << 15) | (k >> (32 - 15))
+		k *= c2
+		h ^= k
+		h = (h << 13) | (h >> (32 - 13))
+		h = (h * 5) + 0xe6546b64
+	}
+	k = 0
+	tailIndex := nblocks * 4
+	switch length & 3 {
+	case 3:
+		k ^= uint32(key[tailIndex+2]) << 16
+		fallthrough
+	case 2:
+		k ^= uint32(key[tailIndex+1]) << 8
+		fallthrough
+	case 1:
+		k ^= uint32(key[tailIndex])
+		k *= c1
+		k = (k << 13) | (k >> (32 - 15))
+		k *= c2
+		h ^= k
+	}
+	h ^= uint32(length)
+	h ^= h >> 16
+	h *= 0x85ebca6b
+	h ^= h >> 13
+	h *= 0xc2b2ae35
+	h ^= h >> 16
+	return h
+}
+
+func Test_Checksum32(t *testing.T) {
+	h32 := Checksum32(blob1)
+	fmt.Printf("Checksum32(\"%v\") = 0x%08x\n", string(blob1), h32)
+	if h32 != 0x1130e7d4 {
+		t.Fail()
+	}
+}
+
+func Test_Checksum32Seed(t *testing.T) {
+	h32 := Checksum32Seed(blob1, 1471)
+	fmt.Printf("Checksum32Seed(\"%v\", 1471) = 0x%08x\n", string(blob1), h32)
+	if h32 != 0xba59a258 {
+		t.Fail()
+	}
+}
+
+func Test_New32(t *testing.T) {
+	var digest = New(0)
+	digest.Write(blob1)
+	digest.Write(blob2)
+	h32 := digest.Sum32()
+	fmt.Printf("Sum32 = 0x%08x\n", h32)
+	if h32 != 0x0d44373a {
+		t.Fail()
+	}
+}
+
+func Test_New32Seed(t *testing.T) {
+	var digest = New(1471)
+	digest.Write(blob1)
+	digest.Write(blob2)
+	h32 := digest.Sum32()
+	fmt.Printf("Sum32 = 0x%08x\n", h32)
+	if h32 != 0x3265e220 {
+		t.Fail()
+	}
+}
+
+func Test_Reset(t *testing.T) {
+	var digest = New(0)
+	digest.Write(blob2)
+	digest.Reset()
+	digest.Write(blob1)
+	h32 := digest.Sum32()
+	fmt.Printf("Sum32 = 0x%08x\n", h32)
+	if h32 != 0x1130e7d4 {
+		t.Fail()
+	}
+}
+
+func Benchmark_xxhash32(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Checksum32(blob1)
+	}
+}
+
+func Benchmark_CRC32IEEE(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		crc32.ChecksumIEEE(blob1)
+	}
+}
+
+func Benchmark_Adler32(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		adler32.Checksum(blob1)
+	}
+}
+
+func Benchmark_Fnv32(b *testing.B) {
+	h := fnv.New32()
+	for i := 0; i < b.N; i++ {
+		h.Sum(blob1)
+	}
+}
+
+func Benchmark_MurmurHash3Hash32(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		mmh3Hash32(blob1)
+	}
+}
