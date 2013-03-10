9638b6226e67baf077da754d7c31f5cd7c36bad3
Vova <vova616@gmail.com>
2013-03-10 14:48:16 +0200

    some more tests just to be sure.

--- xxhash_test.go
+++ xxhash_test.go
@@ -4,7 +4,6 @@ package xxhash
 import (
 	"bytes"
 	"encoding/binary"
-	"fmt"
 	"hash/adler32"
 	"hash/crc32"
 	"hash/fnv"
@@ -14,8 +13,145 @@ import (
 var (
 	blob1       = []byte("Lorem ipsum dolor sit amet, consectetuer adipiscing elit, ")
 	blob2       = []byte("sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.")
+	blob3       = []byte("Cookies")
+	blob4       = []byte("1234567890123456")
 	VeryBigFile = "a-very-big-file"
 )
+
+func Test_Checksum32(t *testing.T) {
+	h32 := Checksum32(blob1)
+	if h32 != 0x1130e7d4 {
+		t.Errorf("Checksum32(\"%v\") = 0x%08x need 0x1130e7d4\n", string(blob1), h32)
+	}
+
+	h32 = Checksum32(blob2)
+	if h32 != 0x24ca2992 {
+		t.Errorf("Checksum32(\"%v\") = 0x%08x need 0x24ca2992\n", string(blob2), h32)
+	}
+
+	h32 = Checksum32(blob3)
+	if h32 != 0x99dd2ca5 {
+		t.Errorf("Checksum32(\"%v\") = 0x%08x need 0x99dd2ca5\n", string(blob3), h32)
+	}
+
+	h32 = Checksum32(blob4)
+	if h32 != 0x03bf5152 {
+		t.Errorf("Checksum32(\"%v\") = 0x%08x need 0x03bf5152\n", string(blob4), h32)
+	}
+}
+
+func Test_Checksum32Seed(t *testing.T) {
+	h32 := Checksum32Seed(blob1, 1471)
+	if h32 != 0xba59a258 {
+		t.Errorf("Checksum32Seed(\"%v\", 1471) = 0x%08x\n need 0xba59a258", string(blob1), h32)
+	}
+
+	h32 = Checksum32Seed(blob2, 1596234)
+	if h32 != 0xf15f3e02 {
+		t.Errorf("Checksum32Seed(\"%v\", 1596234) = 0x%08x need 0xf15f3e02\n", string(blob2), h32)
+	}
+
+	h32 = Checksum32Seed(blob3, 9999666)
+	if h32 != 0xcd3ae44c {
+		t.Errorf("Checksum32Seed(\"%v\", 9999666) = 0x%08x need 0xcd3ae44c\n", string(blob3), h32)
+	}
+
+	h32 = Checksum32Seed(blob4, 1)
+	if h32 != 0x606913c4 {
+		t.Errorf("Checksum32Seed(\"%v\", 1) = 0x%08x need 0x606913c4\n", string(blob4), h32)
+	}
+}
+
+func Test_New32(t *testing.T) {
+	var digest = New(0)
+	digest.Write(blob1)
+	digest.Write(blob2)
+	h32 := digest.Sum32()
+	if h32 != 0x0d44373a {
+		t.Errorf("Sum32 = 0x%08x need 0x0d44373a\n", h32)
+	}
+
+	digest = New(0)
+	digest.Write(blob3)
+	h32 = digest.Sum32()
+	if h32 != 0x99dd2ca5 {
+		t.Errorf("Sum32 = 0x%08x need 0x99dd2ca5\n", h32)
+	}
+
+	digest = New(0)
+	digest.Write(blob4)
+	h32 = digest.Sum32()
+	if h32 != 0x3bf5152 {
+		t.Errorf("Sum32 = 0x%08x need 0x3bf5152\n", h32)
+	}
+}
+
+func Test_New32Seed(t *testing.T) {
+	var digest = New(1471)
+	digest.Write(blob1)
+	digest.Write(blob2)
+	h32 := digest.Sum32()
+	if h32 != 0x3265e220 {
+		t.Errorf("Sum32 = 0x%08x need 0x3265e220\n", h32)
+	}
+
+	digest = New(615324687)
+	digest.Write(blob3)
+	h32 = digest.Sum32()
+	if h32 != 0xb90e95cb {
+		t.Errorf("Sum32 = 0x%08x need 0x89f56371\n", h32)
+	}
+
+	digest = New(1)
+	digest.Write(blob4)
+	h32 = digest.Sum32()
+	if h32 != 0x606913c4 {
+		t.Errorf("Sum32 = 0x%08x need 0x606913c4\n", h32)
+	}
+
+}
+
+func Test_Reset(t *testing.T) {
+	var digest = New(0)
+	digest.Write(blob2)
+	digest.Reset()
+	digest.Write(blob1)
+	h32 := digest.Sum32()
+	if h32 != 0x1130e7d4 {
+		t.Errorf("Sum32 = 0x%08x need 0x1130e7d4\n", h32)
+	}
+}
+
+func Benchmark_xxhash32(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Checksum32(blob1)
+	}
+}
+
+func Benchmark_CRC32IEEE(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		crc32.ChecksumIEEE(blob1)
+	}
+}
+
+func Benchmark_Adler32(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		adler32.Checksum(blob1)
+	}
+}
+
+func Benchmark_Fnv32(b *testing.B) {
+	h := fnv.New32()
+	for i := 0; i < b.N; i++ {
+		h.Sum(blob1)
+	}
+}
+
+func Benchmark_MurmurHash3Hash32(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		mmh3Hash32(blob1)
+	}
+}
 
 // MurmurHash 3
 // mmh3.Hash32 stollen from https://github.com/reusee/mmh3
@@ -60,85 +196,4 @@ func mmh3Hash32(key []byte) uint32 {
 	h *= 0xc2b2ae35
 	h ^= h >> 16
 	return h
-}
-
-func Test_Checksum32(t *testing.T) {
-	h32 := Checksum32(blob1)
-	fmt.Printf("Checksum32(\"%v\") = 0x%08x\n", string(blob1), h32)
-	if h32 != 0x1130e7d4 {
-		t.Fail()
-	}
-}
-
-func Test_Checksum32Seed(t *testing.T) {
-	h32 := Checksum32Seed(blob1, 1471)
-	fmt.Printf("Checksum32Seed(\"%v\", 1471) = 0x%08x\n", string(blob1), h32)
-	if h32 != 0xba59a258 {
-		t.Fail()
-	}
-}
-
-func Test_New32(t *testing.T) {
-	var digest = New(0)
-	digest.Write(blob1)
-	digest.Write(blob2)
-	h32 := digest.Sum32()
-	fmt.Printf("Sum32 = 0x%08x\n", h32)
-	if h32 != 0x0d44373a {
-		t.Fail()
-	}
-}
-
-func Test_New32Seed(t *testing.T) {
-	var digest = New(1471)
-	digest.Write(blob1)
-	digest.Write(blob2)
-	h32 := digest.Sum32()
-	fmt.Printf("Sum32 = 0x%08x\n", h32)
-	if h32 != 0x3265e220 {
-		t.Fail()
-	}
-}
-
-func Test_Reset(t *testing.T) {
-	var digest = New(0)
-	digest.Write(blob2)
-	digest.Reset()
-	digest.Write(blob1)
-	h32 := digest.Sum32()
-	fmt.Printf("Sum32 = 0x%08x\n", h32)
-	if h32 != 0x1130e7d4 {
-		t.Fail()
-	}
-}
-
-func Benchmark_xxhash32(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		Checksum32(blob1)
-	}
-}
-
-func Benchmark_CRC32IEEE(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		crc32.ChecksumIEEE(blob1)
-	}
-}
-
-func Benchmark_Adler32(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		adler32.Checksum(blob1)
-	}
-}
-
-func Benchmark_Fnv32(b *testing.B) {
-	h := fnv.New32()
-	for i := 0; i < b.N; i++ {
-		h.Sum(blob1)
-	}
-}
-
-func Benchmark_MurmurHash3Hash32(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		mmh3Hash32(blob1)
-	}
 }
