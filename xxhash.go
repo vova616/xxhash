4f2f2654c22496893e9334ebf4deedd9a728bcd2
Vova <vova616@gmail.com>
2013-03-14 00:35:43 +0200

    More unsafe code to increase performance.

--- xxhash.go
+++ xxhash.go
@@ -41,34 +41,36 @@ func (self *XXHash) Size() int {
 }
 
 func (self *XXHash) feed(in []byte) uint32 {
-	p := 0
-	bEnd := len(in)
+	p := uintptr(unsafe.Pointer(&in[0]))
+	pTemp := p
+	l := len(in)
+	bEnd := p + uintptr(l)
 
-	self.total_len += uint64(bEnd)
+	self.total_len += uint64(l)
 
 	// fill in tmp buffer
-	if self.memsize+bEnd < 16 {
+	if self.memsize+l < 16 {
 		copy(self.memory[self.memsize:], in)
-		self.memsize += bEnd
+		self.memsize += l
 		return 0
 	}
 
 	if self.memsize > 0 {
 		copy(self.memory[self.memsize:], in[:16-self.memsize])
-		self.v1 += *(*uint32)(unsafe.Pointer(&self.memory[0])) * PRIME32_2
+		p2 := uintptr(unsafe.Pointer(&self.memory[0]))
+		self.v1 += (*(*uint32)(unsafe.Pointer(p2))) * PRIME32_2
 		self.v1 = ((self.v1 << 13) | (self.v1 >> (32 - 13))) * PRIME32_1
 
-		self.v2 += *(*uint32)(unsafe.Pointer(&self.memory[4])) * PRIME32_2
+		self.v2 += (*(*uint32)(unsafe.Pointer(p2 + 4))) * PRIME32_2
 		self.v2 = ((self.v2 << 13) | (self.v2 >> (32 - 13))) * PRIME32_1
 
-		self.v3 += *(*uint32)(unsafe.Pointer(&self.memory[8])) * PRIME32_2
+		self.v3 += (*(*uint32)(unsafe.Pointer(p2 + 8))) * PRIME32_2
 		self.v3 = ((self.v3 << 13) | (self.v3 >> (32 - 13))) * PRIME32_1
 
-		self.v4 += *(*uint32)(unsafe.Pointer(&self.memory[12])) * PRIME32_2
+		self.v4 += (*(*uint32)(unsafe.Pointer(p2 + 12))) * PRIME32_2
 		self.v4 = ((self.v4 << 13) | (self.v4 >> (32 - 13))) * PRIME32_1
 
-		s := 16 - self.memsize
-		p += s
+		p += 16 - uintptr(self.memsize)
 		self.memsize = 0
 	}
 
@@ -76,24 +78,17 @@ func (self *XXHash) feed(in []byte) uint32 {
 	v1, v2, v3, v4 := self.v1, self.v2, self.v3, self.v4
 
 	for ; p <= limit; p += 16 {
-		v1 += *(*uint32)(unsafe.Pointer(&in[p])) * PRIME32_2
+		v1 += (*(*uint32)(unsafe.Pointer(p))) * PRIME32_2
 		v1 = ((v1 << 13) | (v1 >> (32 - 13))) * PRIME32_1
 
-		v2 += *(*uint32)(unsafe.Pointer(&in[p+4])) * PRIME32_2
+		v2 += (*(*uint32)(unsafe.Pointer(p + 4))) * PRIME32_2
 		v2 = ((v2 << 13) | (v2 >> (32 - 13))) * PRIME32_1
 
-		v3 += *(*uint32)(unsafe.Pointer(&in[p+8])) * PRIME32_2
+		v3 += (*(*uint32)(unsafe.Pointer(p + 8))) * PRIME32_2
 		v3 = ((v3 << 13) | (v3 >> (32 - 13))) * PRIME32_1
 
-		v4 += *(*uint32)(unsafe.Pointer(&in[p+12])) * PRIME32_2
+		v4 += (*(*uint32)(unsafe.Pointer(p + 12))) * PRIME32_2
 		v4 = ((v4 << 13) | (v4 >> (32 - 13))) * PRIME32_1
-	}
-
-	limit = bEnd - p
-
-	if limit > 0 {
-		copy(self.memory[:], in[p:bEnd])
-		self.memsize = limit
 	}
 
 	self.v1 = v1
@@ -101,12 +96,19 @@ func (self *XXHash) feed(in []byte) uint32 {
 	self.v3 = v3
 	self.v4 = v4
 
+	limit = bEnd - p
+
+	if limit > 0 {
+		copy(self.memory[:], in[p-pTemp:bEnd-pTemp])
+		self.memsize = int(limit)
+	}
+
 	return 0
 }
 
 func (self *XXHash) Sum32() uint32 {
-	p := 0
-	bEnd := self.memsize
+	p := uintptr(unsafe.Pointer(&self.memory[0]))
+	bEnd := p + uintptr(self.memsize)
 	h32 := uint32(0)
 
 	if self.total_len >= 16 {
@@ -121,13 +123,13 @@ func (self *XXHash) Sum32() uint32 {
 	h32 += uint32(self.total_len)
 
 	for p <= bEnd-4 {
-		h32 += *(*uint32)(unsafe.Pointer(&self.memory[p])) * PRIME32_3
+		h32 += (*(*uint32)(unsafe.Pointer(p))) * PRIME32_3
 		h32 = ((h32 << 17) | (h32 >> (32 - 17))) * PRIME32_4
 		p += 4
 	}
 
 	for p < bEnd {
-		h32 += uint32(self.memory[p]) * PRIME32_5
+		h32 += uint32(*(*byte)(unsafe.Pointer(p))) * PRIME32_5
 		h32 = ((h32 << 11) | (h32 >> (32 - 11))) * PRIME32_1
 		p++
 	}
@@ -163,6 +165,9 @@ func (self *XXHash) Reset() {
 // Write adds more data to the running hash.
 // Length of data MUST BE less than 1 Gigabytes.
 func (self *XXHash) Write(data []byte) (nn int, err error) {
+	if data == nil {
+		return 0, errors.New("Data cannot be nil.")
+	}
 	l := len(data)
 	if l > 1<<30 {
 		return 0, errors.New("Cannot add more than 1 Gigabytes at once.")
@@ -178,11 +183,15 @@ func Checksum32(data []byte) uint32 {
 
 // Checksum32 returns the xxhash32 checksum of data. Length of data MUST BE less than 2 Gigabytes.
 func Checksum32Seed(data []byte, seed uint32) uint32 {
-	p := 0
-	bEnd := len(data)
+	if data == nil {
+		panic("Data cannot be nil.")
+	}
+	p := uintptr(unsafe.Pointer(&data[0]))
+	l := len(data)
+	bEnd := p + uintptr(l)
 	h32 := uint32(0)
 
-	if bEnd >= 16 {
+	if l >= 16 {
 		limit := bEnd - 16
 
 		v1 := seed + PRIME32_1 + PRIME32_2
@@ -190,16 +199,16 @@ func Checksum32Seed(data []byte, seed uint32) uint32 {
 		v3 := seed + 0
 		v4 := seed - PRIME32_1
 		for {
-			v1 += *(*uint32)(unsafe.Pointer(&data[p])) * PRIME32_2
+			v1 += (*(*uint32)(unsafe.Pointer(p))) * PRIME32_2
 			v1 = ((v1 << 13) | (v1 >> (32 - 13))) * PRIME32_1
 
-			v2 += *(*uint32)(unsafe.Pointer(&data[p+4])) * PRIME32_2
+			v2 += (*(*uint32)(unsafe.Pointer(p + 4))) * PRIME32_2
 			v2 = ((v2 << 13) | (v2 >> (32 - 13))) * PRIME32_1
 
-			v3 += *(*uint32)(unsafe.Pointer(&data[p+8])) * PRIME32_2
+			v3 += (*(*uint32)(unsafe.Pointer(p + 8))) * PRIME32_2
 			v3 = ((v3 << 13) | (v3 >> (32 - 13))) * PRIME32_1
 
-			v4 += *(*uint32)(unsafe.Pointer(&data[p+12])) * PRIME32_2
+			v4 += (*(*uint32)(unsafe.Pointer(p + 12))) * PRIME32_2
 			v4 = ((v4 << 13) | (v4 >> (32 - 13))) * PRIME32_1
 
 			p += 16
@@ -215,16 +224,16 @@ func Checksum32Seed(data []byte, seed uint32) uint32 {
 		h32 = seed + PRIME32_5
 	}
 
-	h32 += uint32(bEnd)
+	h32 += uint32(l)
 
 	for p <= bEnd-4 {
-		h32 += *(*uint32)(unsafe.Pointer(&data[p])) * PRIME32_3
+		h32 += (*(*uint32)(unsafe.Pointer(p))) * PRIME32_3
 		h32 = ((h32 << 17) | (h32 >> (32 - 17))) * PRIME32_4
 		p += 4
 	}
 
 	for p < bEnd {
-		h32 += uint32(data[p]) * PRIME32_5
+		h32 += uint32(*(*byte)(unsafe.Pointer(p))) * PRIME32_5
 		h32 = ((h32 << 11) | (h32 >> (32 - 11))) * PRIME32_1
 		p++
 	}
